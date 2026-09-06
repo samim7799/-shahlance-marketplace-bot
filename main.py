cf360404f4033bb003a3b64adb8c746049f80793
samim7799 <94230460+samim7799@users.noreply.github.com>
2026-09-06 06:11:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ dp = Dispatcher()
 WELCOME_TEXT = (
     "🌟Welcome To ShahLance.Com\n\n"
     "🛍️All in One Digital Marketplace\n\n\n"
-    "🥰 Satisfaction  Guarantee\n"
+    "🥰Satisfaction Guarantee\n"
     "🖇️100% Escrow Security\n"
-    "🔒 Secure & Verified Deals\n"
-    "⚡ Instant & Auto  Delivery\n"
-    " ✅ Verified  & Safe  Sellers\n"
-    " 🧑🏼‍💻 24/7 Live OnlineSupport"
+    "🔒Secure & Verified Deals\n"
+    "⚡Instant & Auto Delivery\n"
+    "✅Verified  & Safe Sellers\n"
+    "🧑🏼‍💻24/7 Live OnlineSupport"
 )
 
 
